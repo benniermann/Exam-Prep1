706d2e8b2cc9b226328d6bae9ab304328c76cf04
Ben <niermannben06@gmail.com>
2026-04-14 14:43:52 +0200

    Implement initial warehouse management functions

---
+++ inventory.c
@@ -0,0 +1,46 @@
+#include "inventory.h"
+#include <stdlib.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+Warehouse *warehouse_create(void)
+{
+
+}
+
+void warehouse_destroy(Warehouse *w)
+{
+
+}
+
+int warehouse_add_product(Warehouse *w, const char *name, double price, int quantity)
+{
+
+}
+
+int warehouse_remove_by_id(Warehouse *w, int id)
+{
+
+}
+
+Product *warehouse_find_by_name(Warehouse *w, const char *query)
+{
+
+}
+
+double warehouse_total_value(const Warehouse *w)
+{
+
+}
+
+void warehouse_sort_by_price(Warehouse *w)
+{
+
+}
+
+void warehouse_print(const Warehouse *w)
+{
+    
+}
